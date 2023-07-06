7006cf5347a02eb33b10e8b2f266514e22ce809b
LUIS <luis.enrique.enc@gmail.com>
2023-07-06 15:55:18 -0600

    [ADD] SE CREAN VISTAS PARA TAB BAR

---
+++ foodAppTapBar/Controller/TabBarController/TabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  TabBarController.swift
+//  foodAppTapBar
+//
+//  Created by LUIS GONZALEZ on 06/07/23.
+//
+
+import UIKit
+
+class TabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.viewControllers = [
+           
+            self.createNavCont(for: OptionMenuViewController(), title: "First Tap Bar", image: UIImage(systemName: "house") ?? UIImage()),
+            self.createNavCont(for: SearchResultViewController(), title: "Second Tap Bar", image: UIImage(systemName: "house") ?? UIImage()),
+        ]
+        
+    }
+    
+    func createNavCont(for mainViewController: UIViewController, title: String, image: UIImage ) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: mainViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        mainViewController.navigationItem.title = title
+        mainViewController.navigationItem.backButtonTitle = "Atras"
+        
+        return navController
+    }
+
+ 
+
+}
+
+
+
