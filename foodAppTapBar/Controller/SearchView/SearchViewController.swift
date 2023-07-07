1acbda97824823583a2a3f39dbbb2664637ca646
LUIS <luis.enrique.enc@gmail.com>
2023-07-07 09:01:58 -0600

    [ADD] se agregan outlets en search view

--- foodAppTapBar/Controller/SearchView/SearchViewController.swift
+++ foodAppTapBar/Controller/SearchView/SearchViewController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    //MARK: - OUTLETS
+    
+    @IBOutlet weak var lblSearchTitle: UILabel!
+    @IBOutlet weak var tableViewSearch: UITableView!
+    
+    
 
+    //MARK: - LIFE - CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      
     }
 
+    
+    
+    //MARK: - FUNCTIONS
+    
+    
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
