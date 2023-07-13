1edd0fd4b0ea167c92e9ee7867ea6dbe9889023b
LUIS <luis.enrique.enc@gmail.com>
2023-07-13 09:11:44 -0600

    [ADD] Primer scroll view

--- foodAppTapBar/Controller/SearchResult/SearchResultViewController.swift
+++ foodAppTapBar/Controller/SearchResult/SearchResultViewController.swift
@@ -11,19 +11,7 @@ class SearchResultViewController: UIViewController {
     
     //MARK: - OUTLETS
     
-    @IBOutlet weak var lblSearchResult: UILabel!
-    @IBOutlet weak var imgSearchResult: UIImageView!
-    @IBOutlet weak var lblImageSR: UILabel!
-    @IBOutlet weak var backgroundSearchR: UIView!
-    @IBOutlet weak var lblPriceSR: UILabel!
-    @IBOutlet weak var starImageSR: UIImageView!
-    @IBOutlet weak var lblStars: UILabel!
-    @IBOutlet weak var clockImageSR: UIImageView!
-    @IBOutlet weak var clockLblSR: UILabel!
-    @IBOutlet weak var descriptionLabelSR: UILabel!
-    @IBOutlet weak var findLablSR: UILabel!
-    @IBOutlet weak var collectionViewSR: UICollectionView!
-    @IBOutlet weak var addButtonSR: UIButton!
+    
     
     
     
