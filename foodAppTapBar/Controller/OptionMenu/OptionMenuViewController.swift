e58dc583163c42fcc35d73aa553fb2a5a56fb97c
LUIS <luis.enrique.enc@gmail.com>
2023-07-13 16:03:29 -0600

    [ADD] outlets de option menu View controller

--- foodAppTapBar/Controller/OptionMenu/OptionMenuViewController.swift
+++ foodAppTapBar/Controller/OptionMenu/OptionMenuViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class OptionMenuViewController: UIViewController {
+    
+    
+    
+    //MARK: - OUTLETS
+    
+    @IBOutlet weak var generlaBackground: UIView!
+    @IBOutlet weak var backfrundSearch: NSLayoutConstraint!
+    @IBOutlet weak var searchBar: UITextField!
+    @IBOutlet weak var recentUserLbl: UILabel!
+    @IBOutlet weak var fisrtCollectionView: UICollectionView!
+    @IBOutlet weak var top3Bundle: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var peolpeNearbyLbl: UILabel!
+    @IBOutlet weak var secondCollectionView: UICollectionView!
+    @IBOutlet weak var btnSearchCat: UIButton!{
+        didSet{
+            self.btnSearchCat.layer.cornerRadius = 10
+        }
+    }
+    
+    //MARK: - VARIABLES
+    
+    
+    
 
+
+    //MARK: - LIFE - CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 
